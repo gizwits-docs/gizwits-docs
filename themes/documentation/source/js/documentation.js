7d5507e6c5e0d96fd6bf7694297cb040a90c19f6
Shuaibird <shuaibird@icloud.com>
2016-10-18 10:51:31 +0800

    add cookies fallback

--- themes/documentation/source/js/documentation.js
+++ themes/documentation/source/js/documentation.js
@@ -21,19 +21,37 @@
 
 // index redirect
 +function() {
-  var browserLang = /zh/.test(navigator.userLanguage || navigator.language) ? 'zh-cn' : 'en-us'
   var lang
 
-  if (localStorage) {
-    var prefLang = localStorage.getItem('lang')
-    if (prefLang) {
+  function getCookie(name) {
+    var value = '; ' + document.cookie
+    var parts = value.split('; ' + name + '=')
+    if (parts.length == 2)
+      return parts.pop().split(';').shift()
+  }
+
+  function getLang(prefLang, savePref) {
+    var browserLang = /zh/.test(navigator.userLanguage || navigator.language) ? 'zh-cn' : 'en-us'
+    if (/zh-cn/.test(prefLang) || /en-us/.test(prefLang)) {
       lang = prefLang
     } else {
       lang = browserLang
+      savePref(lang)
+    }
+  }
+
+  if (localStorage) {
+    var prefLang = localStorage.getItem('lang')
+    var setStorage = function(lang) {
       localStorage.setItem('lang', lang)
     }
+    getLang(prefLang, setStorage)
   } else {
-    lang = browserLang
+    var prefLang = getCookie('lang')
+    var setCookie = function(lang) {
+      document.cookie = 'lang=' + lang + '; expires=Fri, 31 Dec 9999 23:59:59 GMT'
+    }
+    getLang(prefLang, setCookie)
   }
 
   if (location.pathname === '/')
@@ -53,6 +71,8 @@
 
       if (localStorage) {
         localStorage.setItem('lang', lang)
+      } else {
+        document.cookie = 'lang=' + lang + '; expires=Fri, 31 Dec 9999 23:59:59 GMT'
       }
     })
   })
