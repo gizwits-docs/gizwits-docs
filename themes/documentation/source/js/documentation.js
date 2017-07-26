e56425e3eff19a75bc1b0c2f9682067fce6b17a1
Carrey Lee <xkli@gizwits.com>
2017-07-26 16:31:09 +0800

    文档中心导航增加切换至其他中心功能 (Feature #6504)

--- themes/documentation/source/js/documentation.js
+++ themes/documentation/source/js/documentation.js
@@ -309,3 +309,11 @@ var HEADER_BP = 915
     var spinner = new Spinner(opts).spin(target)
   }
 }()
+
+
+$(function() {
+  $('body').on('click', '#sub-title', function(e) {
+    e.preventDefault()
+    $('#sub-title-menu').toggle()
+  })
+})
