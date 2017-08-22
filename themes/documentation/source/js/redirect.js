c815afcad473ae5f4a6f4b72dc932c9c6be51b6c
hohuiking <524755365@qq.com>
2017-08-22 17:43:29 +0800

    新增重定向

--- themes/documentation/source/js/redirect.js
+++ themes/documentation/source/js/redirect.js
@@ -1,8 +1,13 @@
 var redirectMap = {
   // 在这里添加重定向，注意逗号
 
-  '/zh-cn/old.html': '/zh-cn/new.html',
-  '/zh-cn/旧.html': '/zh-cn/新.html'
+//  '/zh-cn/old.html': '/zh-cn/new.html',
+//  '/zh-cn/旧.html': '/zh-cn/新.html'
+
+'/zh-cn/quickstart/独立mcu接入方案.html':'/zh-cn/quickstart/UseMCU.html',
+'/zh-cn/quickstart/设备快速接入.html':'/zh-cn/quickstart/UseMCU.html',
+'/zh-cn/UserManual/使用设备分组API教程.html':'/zh-cn/UserManual/devgroup_API.html'
+
 }
 
 var pathname = decodeURI(window.location.pathname)
