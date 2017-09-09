66d4f8a63f473bfd00ca2e66dbd0152327f5e808
hohuiking <524755365@qq.com>
2017-09-09 16:34:36 +0800

    重定向

--- themes/documentation/source/js/redirect.js
+++ themes/documentation/source/js/redirect.js
@@ -7,8 +7,7 @@ var redirectMap = {
 '/zh-cn/quickstart/独立mcu接入方案.html':'/zh-cn/quickstart/UseMCU.html',
 '/zh-cn/quickstart/设备快速接入.html':'/zh-cn/quickstart/UseMCU.html',
 '/zh-cn/UserManual/使用设备分组API教程.html':'/zh-cn/UserManual/devgroup_API.html',
-'/zh-cn/UserManual/OTA.html':'/zh-cn/UserManual/OTA使用教程.html',
-'/zh-cn/AppDev/third-party.html':'/zh-cn/AppDev/iOS第三方登陆与换肤.html'
+'/zh-cn/UserManual/OTA.html':'/zh-cn/UserManual/OTA使用教程.html'
 }
 
 var pathname = decodeURI(window.location.pathname)
